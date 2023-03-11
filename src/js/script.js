7ab514b3534a2945a7293a249f903142f473910a
Kiril Dudnik <kdudnik17@gmail.com>
2023-03-11 15:47:26 +0200

    Refactor switch theme

--- src/js/script.js
+++ src/js/script.js
@@ -1,60 +1,8 @@
-// import "../scss/main.scss"
 import initDropzone from "./csvUpload.js"
+import initThemeSwitcher from "./switchTheme.js"
 
+initThemeSwitcher()
 initDropzone()
-
-const storageKey = "theme-mode" // just a name of the field
-
-// Color scheme
-window.onload = () => {
-  // set on load so screen readers can see latest value on the button
-  reflectPreference()
-
-  // now this script can find and listen for clicks on the control
-  document.querySelector("#switchColorScheme").addEventListener("click", () => {
-    // flip current value
-    theme.value = theme.value === "light" ? "dark" : "light"
-
-    setPreference()
-  })
-}
-
-const getColorPreference = () => {
-  if (localStorage.getItem(storageKey)) {
-    return localStorage.getItem(storageKey)
-  } else {
-    return window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light"
-  }
-}
-
-const theme = {
-  value: getColorPreference(),
-}
-
-const setPreference = () => {
-  localStorage.setItem(storageKey, theme.value)
-  reflectPreference()
-}
-
-const reflectPreference = () => {
-  document.firstElementChild.setAttribute("data-theme", theme.value)
-  document
-    .querySelector("#switchColorScheme")
-    ?.setAttribute("aria-label", theme.value)
-}
-
-// set early so no page flashes / CSS is made aware
-reflectPreference()
-
-// sync with system changes
-window
-  .matchMedia("(prefers-color-scheme: dark)")
-  .addEventListener("change", ({ matches: isDark }) => {
-    theme.value = isDark ? "dark" : "light"
-    setPreference()
-  })
 
 // --------- CARDS -------
 const cards = document.querySelectorAll(".card")
---
+++ src/js/switchTheme.js
@@ -0,0 +1,47 @@
+const storageKey = "theme-mode" // just a name of the field
+
+const switcher = document.querySelector("#switchColorScheme")
+
+const initThemeSwitcher = () => {
+  reflectPreference()
+  switcher.addEventListener("click", () => {
+      // flip current value
+      theme.value = theme.value === "light" ? "dark" : "light"
+  
+      setPreference()
+  })
+
+  // sync with system changes
+  window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", ({ matches: isDark }) => {
+      theme.value = isDark ? "dark" : "light"
+      setPreference()
+  })
+}
+
+const getColorPreference = () => {
+    if (localStorage.getItem(storageKey)) {
+        return localStorage.getItem(storageKey)
+    } else {
+        return window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light"
+    }
+}
+
+const theme = {
+    value: getColorPreference(),
+}
+
+const setPreference = () => {
+    localStorage.setItem(storageKey, theme.value)
+    reflectPreference()
+}
+
+const reflectPreference = () => {
+    document.firstElementChild.setAttribute("data-theme", theme.value)
+    switcher?.setAttribute("aria-label", theme.value)
+}
+
+export default initThemeSwitcher
