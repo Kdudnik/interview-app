2c772e451cf3a61943dfc603207171d434cdbd97
Kiril Dudnik <kdudnik17@gmail.com>
2023-04-16 23:59:43 +0300

    Add responsive result form  - add switch summary/score

--- README.md
+++ README.md
@@ -49,6 +49,7 @@ The Interview App is a web application designed to streamline the interview proc
 | Add result form                       | ✅ Completed   | 2023-04-16                        |
 | Change color on state of progress bar | ✅ Completed   | 2023-04-16                        |
 | Show results                          | ⏳ In Progress | TBD                               |
+| Improve questions generation          | 🚧 Not Started | TBD                               |
 | Add back side of each card            | 🚧 Not Started | TBD                               |
 | Error for non-CSV on mobile           | 🚧 Not Started | TBD                               |
 | Give each question a price            | 🚧 Not Started | TBD                               |
--- src/js/result.js
+++ src/js/result.js
@@ -1,28 +1,31 @@
 import { topicSVGs } from "./utils/topicsSVG"
 
-let inputTest = document.querySelector('#inputTest')
-let submitTest = document.querySelector('#submitTest')
+// let inputTest = document.querySelector('#inputTest')
+// let submitTest = document.querySelector('#submitTest')
 const resultTitle = document.querySelector('.score h5')
-const progressBar = document.querySelector('.card progress')
-const summaryList = document.querySelector('.summary')
+const summary = document.querySelector('.summary')
+const score = document.querySelector('.result .card')
+const progressBar = score.querySelector('progress')
+const showSummary = document.querySelector('.show__summary')
+const showScore = document.querySelector('.show__score')
 
-submitTest.addEventListener('click', () => {
-    progressBar.value = inputTest.value
-    resultTitle.innerHTML = inputTest.value
+// submitTest.addEventListener('click', () => {
+//     progressBar.value = inputTest.value
+//     resultTitle.innerHTML = inputTest.value
 
-    if(inputTest.value >= 0 && inputTest.value <= 33) {
-        progressBar.dataset.scoreResult = "bad"
-    } else if(inputTest.value >= 34 && inputTest.value <= 66) {
-        progressBar.dataset.scoreResult = "average"
-    } else if(inputTest.value >= 67 && inputTest.value <= 100) {
-        progressBar.dataset.scoreResult = "good"
-    }
+//     if(inputTest.value >= 0 && inputTest.value <= 33) {
+//         progressBar.dataset.scoreResult = "bad"
+//     } else if(inputTest.value >= 34 && inputTest.value <= 66) {
+//         progressBar.dataset.scoreResult = "average"
+//     } else if(inputTest.value >= 67 && inputTest.value <= 100) {
+//         progressBar.dataset.scoreResult = "good"
+//     }
 
-    if(resultTitle.innerHTML > 100) {
-        resultTitle.innerHTML = 100
-        console.error("Result is more than 100")
-    }
-})
+//     if(resultTitle.innerHTML > 100) {
+//         resultTitle.innerHTML = 100
+//         console.error("Result is more than 100")
+//     }
+// })
 
 function generateSummary(topics) {
     let summaryArr = []
@@ -35,10 +38,24 @@ function generateSummary(topics) {
                 <span>${topicSVGs[field.toLowerCase()]} ${field}</span>
                 <span>50<span class="summary__score--static">/100</span></span>
             </div>`
-        summaryList.insertAdjacentHTML("beforeend", summaryEl)
+        summary.insertAdjacentHTML("beforeend", summaryEl)
         summaryArr.push(field)
     })
     return summaryArr
 }
+
+showSummary.addEventListener('click', () => {
+    summary.style.display = "flex"
+    summary.style.animation = "resultShow .5s"
+    score.style.animation = "resultHide .5s"
+    score.style.display = "none"
+})
+
+showScore.addEventListener('click', () => {
+    summary.style.animation = "resultHide .5s"
+    summary.style.display = "none"
+    score.style.display = "flex"
+    score.style.animation = "resultShow .5s"
+})
 
 export { generateSummary }
